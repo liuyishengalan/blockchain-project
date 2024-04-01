7b9034db8275b09f39cf7a56d7f8ff3047e57ae1
liuyishengalan <liuyishengalan@hotmail.com>
2024-04-01 03:14:05 -0700

    add particles background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,133 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
 import styled from 'styled-components';
-import { ActivateDeactivate } from './components/ActivateDeactivate';
-import { Greeter } from './components/Greeter';
-import { SectionDivider } from './components/SectionDivider';
-import { SignMessage } from './components/SignMessage';
-import { WalletStatus } from './components/WalletStatus';
 import { Lotto } from './components/Lotto';
+import { loadSlim } from "@tsparticles/slim";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
 
 const StyledAppDiv = styled.div`
+  font-family: Arial, sans-serif;
+  font-size: 1.2em;
   display: grid;
   grid-gap: 20px;
+  position: relative;
+  z-index: 1;
 `;
 
 export function App(): ReactElement {
-  return (
-    <StyledAppDiv>
-      <Lotto />
-
-      
-      {/* <ActivateDeactivate /> */}
-      {/* <SectionDivider /> */}
-      {/* <WalletStatus /> */}
-      {/* <SectionDivider /> */}
-      {/* <SignMessage /> */}
-      {/* <SectionDivider /> */}
-      {/* <Greeter /> */}
-    </StyledAppDiv>
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+  
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+  
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#7f7f7f",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 200,
+          enable: true,
+          opacity: 0.7,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 4,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
   );
+
+  if (init) {
+    return (
+      <>
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options as unknown as ISourceOptions}
+        />
+        <StyledAppDiv>
+          <Lotto />
+        </StyledAppDiv>
+      </>
+    );
+  }
+
+  return (
+  <StyledAppDiv>
+    <Lotto />
+  </StyledAppDiv>
+  )
 }
